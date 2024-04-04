0ad7ca94649cd6cdb982038b9f702bff49369fc7
Belinda Caylor <belinda@thekestrel.co>
2024-04-04 09:18:57 -0500

    Dev setup

--- bc-schedule.php
+++ bc-schedule.php
@@ -30,8 +30,8 @@ function bc_schedule_enqueue_admin_assets( $hook ) {
     // Enqueue the bundled JavaScript file
     wp_enqueue_script(
         'bc-schedule-admin-main',
-        // BC_SCHEDULE_URL . 'dist/admin.js',
-        BC_SCHEDULE_URL . 'dist/admin.min.js',
+        BC_SCHEDULE_URL . 'dist/admin.js',
+        // BC_SCHEDULE_URL . 'dist/admin.min.js',
         array(),
         '1.0.0',
         true
@@ -42,8 +42,8 @@ function bc_schedule_enqueue_admin_assets( $hook ) {
     if ($current_screen->id === 'toplevel_page_volunteer-schedule') {
         wp_enqueue_style(
             'bc-schedule-admin-styles',
-            // BC_SCHEDULE_URL . 'dist/stylesheet.css',
-            BC_SCHEDULE_URL . 'dist/stylesheet.min.9c922cb195f5d880d1db.css',
+            BC_SCHEDULE_URL . 'dist/stylesheet.css',
+            // BC_SCHEDULE_URL . 'dist/stylesheet.min.9c922cb195f5d880d1db.css',
             array(),
             '1.0.0'
         );
@@ -69,8 +69,8 @@ add_action( 'admin_enqueue_scripts', 'bc_schedule_enqueue_admin_assets' );
 function enqueue_bcs_frontend_scripts() {
     wp_enqueue_style(
         'bc-schedule-styles',
-        // BC_SCHEDULE_URL . 'dist/stylesheet.css',
-        BC_SCHEDULE_URL . 'dist/stylesheet.min.9c922cb195f5d880d1db.css',
+        BC_SCHEDULE_URL . 'dist/stylesheet.css',
+        // BC_SCHEDULE_URL . 'dist/stylesheet.min.9c922cb195f5d880d1db.css',
         array(),
         '1.0.0'
     );
