2958b18e0e691db619d82c961dc9b69441397bb3
Belinda Caylor <belinda@thekestrel.co>
2024-04-15 16:18:22 -0500

    Fix schedule shortcode

--- bc-schedule.php
+++ bc-schedule.php
@@ -3,7 +3,7 @@
  * 
  * Plugin Name: Schedule Manager
  * Description: Church Volunteer Schedule.
- * Version: 2.0.1
+ * Version: 2.0.2
  * Author: Belinda Caylor
 */
 
@@ -44,7 +44,7 @@ function bc_schedule_enqueue_admin_assets( $hook ) {
         BC_SCHEDULE_URL . 'dist/admin.js',
         // BC_SCHEDULE_URL . 'dist/admin.min.js',
         array(),
-        '2.0.1',
+        '2.0.2',
         true
     );
 
@@ -56,7 +56,7 @@ function bc_schedule_enqueue_admin_assets( $hook ) {
             BC_SCHEDULE_URL . 'dist/stylesheet.css',
             // BC_SCHEDULE_URL . 'dist/stylesheet.min.0787e61bea0d3ebf4aea.css',
             array(),
-            '2.0.1'
+            '2.0.2'
         );
     }
 
@@ -70,10 +70,10 @@ add_action( 'admin_enqueue_scripts', 'bc_schedule_enqueue_admin_assets' );
 function enqueue_bcs_frontend_scripts() {
     wp_enqueue_style(
         'bc-schedule-styles',
-        // BC_SCHEDULE_URL . 'dist/stylesheet.css',
-        BC_SCHEDULE_URL . 'dist/stylesheet.min.0787e61bea0d3ebf4aea.css',
+        BC_SCHEDULE_URL . 'dist/stylesheet.css',
+        // BC_SCHEDULE_URL . 'dist/stylesheet.min.0787e61bea0d3ebf4aea.css',
         array(),
-        '1.0.0'
+        '2.0.2'
     );
 
     $bcs_frontend_data = array(
--- src/frontend/schedule.php
+++ src/frontend/schedule.php
@@ -13,64 +13,66 @@ function render_schedule_frontend() {
 
     if ($all_events) {
         $all_schedule_roles = $all_schedule['schedule'];
-        $all_volunteers_data = $all_schedule['allVolunteers'];
         $exclude_dates = $all_schedule['excludeDates'];
 
         ?>
         <div class="bcs-schedule">
-            <div class="flex justify-between items-center">
-                <h2>Upcoming Schedule</h2>
-                <a href="/wp-admin/admin.php?page=volunteer-schedule" class="bg-blue-800 text-white rounded-md px-4 hover:bg-blue-500 hover:text-white">Edit Schedule</a>
-            </div>
-            <div class="table-wrapper">
-                <table>
-                    <thead class="text-left">
-                        <tr class="sticky top-0 left-0">
-                            <th class="col-1 sticky top-0 left-0">Role</th>
-                            <?php foreach ($all_events as $event) : ?>
-                                <th>
-                                    <div><?php echo date('M j', strtotime($event->date)); ?></div>
-                                    <div><?php echo $event->name; ?></div>
-                                </th>
-                            <?php endforeach; ?>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php foreach ($all_schedule_roles as $group => $group_roles) : ?>
-                            <?php foreach ($group_roles as $role => $role_events) : ?>
-                                <tr>
-                                    <td class="col-1 sticky top-0 left-0 font-semibold"><?php echo $role; ?></td>
-                                    <?php foreach ($all_events as $event) : ?>
-                                        <td>
-                                            <?php
-                                            $volunteer = isset($role_events[$event->id]) ? $role_events[$event->id] : null;
-                                            if ($volunteer) {
-                                                echo $volunteer->first_name;
-                                            }
-                                            ?>
-                                        </td>
-                                    <?php endforeach; ?>
+            <?php foreach ($all_schedule_roles as $event_type => $events) : ?>
+                <h2><?php echo $event_type ?></h2>
+                <div class="table-wrapper">
+                    <table>
+                        <thead class="text-left">
+                            <tr class="sticky top-0 left-0">
+                                <th class="col-1 sticky top-0 left-0">Role</th>
+                                <?php foreach ($all_events as $event) : ?>
+                                    <th>
+                                        <div><?php echo date('M j', strtotime($event->date)); ?></div>
+                                    </th>
+                                <?php endforeach; ?>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach ($events as $group => $group_roles) : ?>
+                                <tr class="border-t border-gray-200 text-white bg-[#005878]">
+                                    <th colspan="<?php echo (count($all_events) + 1); ?>" scope="colgroup" class="!text-white bg-[#005878] col-1 sticky top-0 left-0 py-2 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-3">
+                                        <?php echo $group; ?>
+                                    </th>
                                 </tr>
+                                <?php foreach ($group_roles as $role => $role_events) : ?>
+                                    <tr class="bg-white">
+                                        <td class="col-1 sticky top-0 left-0 font-semibold"><?php echo $role; ?></td>
+                                        <?php foreach ($all_events as $event) : ?>
+                                            <td>
+                                                <?php
+                                                $volunteer = isset($role_events[$event->id]) ? $role_events[$event->id] : null;
+                                                if ($volunteer) {
+                                                    echo $volunteer->first_name;
+                                                }
+                                                ?>
+                                            </td>
+                                        <?php endforeach; ?>
+                                    </tr>
+                                <?php endforeach; ?>
                             <?php endforeach; ?>
-                        <?php endforeach; ?>
-                        <tr>
-                            <td class="col-1 sticky top-0 left-0 font-semibold">Exclude</td>
-                            <?php foreach ($all_events as $event) : ?>
-                                <td>
-                                    <div class="flex flex-wrap">
-                                        <?php
-                                            $date = date("Y-m-d", strtotime($event->date));
-                                            foreach  ($exclude_events['excludeDatesbyDate'][$date] as $name) :
-                                                echo '<div class="list-names text-xs pr-2">' . $name . '</div>';
-                                            endforeach;
-                                        ?>
-                                    </div>
-                                </td>
-                            <?php endforeach; ?>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+                            <tr class="bg-gray-100">
+                                <td class="col-1 sticky top-0 left-0 font-semibold">Exclude</td>
+                                <?php foreach ($all_events as $event) : ?>
+                                    <td class="max-w-20">
+                                        <div class="flex flex-wrap">
+                                            <?php
+                                                $date = date("Y-m-d", strtotime($event->date));
+                                                foreach  ($exclude_events['excludeDatesbyDate'][$date] as $name) :
+                                                    echo '<div class="list-names text-xs pr-2">' . $name . '</div>';
+                                                endforeach;
+                                            ?>
+                                        </div>
+                                    </td>
+                                <?php endforeach; ?>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            <?php endforeach; ?>
         </div>
         <?php
     } else {
